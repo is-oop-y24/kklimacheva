9c6c2939edcf035279af328a5380e5f6a642a1e4
kklimacheva <kateklimacheva02@gmail.com>
2022-03-21 22:45:42 +0300

    lab-3

--- Backups.Tests/Program.cs
+++ Backups.Tests/Program.cs
@@ -18,7 +18,7 @@ namespace Backups.Tests
         {
             var obj1 = new JobObject("C:\\Users\\kklimacheva\\FilesToAdd\\a.txt");
             var obj2 = new JobObject("C:\\Users\\kklimacheva\\FilesToAdd\\b.txt");
-            List<JobObject> objects = new();
+            var objects = new List<JobObject>();
             objects.Add(obj1);
             objects.Add(obj2);
             IStorageAlgorithm type = new SplitStorage(_newRepo);
